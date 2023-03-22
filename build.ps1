e2efef31026307a2d158507c44045397e41b2ac6
Andrii Kurdiumov <kant2002@gmail.com>
2023-03-22 20:51:36 +0600

    Update sample with changes required to make 8.x ILC LLVM works.

---
+++ build.ps1
@@ -0,0 +1,8 @@
+rm bin,obj,.packages -Recurse -Force
+dotnet restore
+cp Microsoft.DotNet.ILCompiler.LLVM.props .packages\microsoft.dotnet.ilcompiler.llvm\8.0.0-alpha.1.23170.1\build\
+cp Microsoft.DotNet.ILCompiler.SingleEntry.targets .packages\microsoft.dotnet.ilcompiler.llvm\8.0.0-alpha.1.23170.1\build\
+# That's important to changes to packages picked up. I did not realize that this is need.
+rm bin,obj -Recurse -Force
+& $env:EMSDK_HOME\emsdk.ps1 activate 3.1.23
+dotnet publish
